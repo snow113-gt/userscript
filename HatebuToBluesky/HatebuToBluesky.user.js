431ed2f455faa090dbecdd34cbd65c590e2daade
snow113-gt <159784268+snow113-gt@users.noreply.github.com>
2024-02-23 15:36:36 +0900

    Update HatebuToBluesky.user.js

--- HatebuToBluesky/HatebuToBluesky.user.js
+++ HatebuToBluesky/HatebuToBluesky.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         hatebuToBluesky
 // @namespace    http://tampermonkey.net/snow113/hatebuToBluesky/
-// @version      2024-02-18
+// @version      2024-02-12
 // @updateURL    https://github.com/snow113-gt/userscript/blob/master/HatebuToBluesky/HatebuToBluesky.user.js
 // @description  はてなブックマークのブックマーク内容をBlueskyに投稿するユーザースクリプト
 // @author       snow113
@@ -9,9 +9,6 @@
 // @icon         https://bsky.app/static/favicon-16x16.png
 // @grant        GM_setValue
 // @grant        GM_xmlhttpRequest
-// @connect      bsky.social
-// @connect      cdn-ak-scissors.b.st-hatena.com
-// @connect      cdn-ak2.favicon.st-hatena.com
 // ==/UserScript==
 
 // Blueskyのユーザー名: example.bsky.social
@@ -48,20 +45,22 @@ const BUTTON_IMAGE = "https://bsky.app/static/favicon-16x16.png";
  * @return 投稿用ノード
  */
 function createBaseIcon(bookmarkNode) {
+    const postData = createPostData(bookmarkNode);
+
     var buttonNode = document.createElement("input");
     buttonNode.setAttribute("type","image");
     buttonNode.setAttribute("src", BUTTON_IMAGE);
-    buttonNode.addEventListener("click", ()=> {return postBluesky(bookmarkNode); }, false);
+    buttonNode.addEventListener("click", ()=> {return postBluesky(postData); }, false);
 
     return buttonNode;
 }
 
 /**
  * Blueskyに投稿する
- * @param bookmarkNode ブックマークアイテムのノード
+ * @param postData 投稿用データ
  * @return 実行結果
  */
-async function postBluesky(bookmarkNode) {
+async function postBluesky(postData) {
     try {
         let client = new BlueskyConnect(BLUESKY_HANDLE, BLUESKY_APP_PASS);
         await client.verify_session()
@@ -73,42 +72,24 @@ async function postBluesky(bookmarkNode) {
             GM_setValue('bsky_session', session);
         });
 
-        let postData = {};
-        try {
-            postData = await createBookmarkPostData(bookmarkNode);
-        } catch (error) {
-            // リンクカード形式にできない時は通常のテキストとして投稿
-            console.log("リンクカードの生成に失敗："+error);
-            postData = createTextPostData(bookmarkNode);
-        }
+        const jsonText = {
+            method: "POST",
+            url: BLUESKY_PDS_URL + '/xrpc/com.atproto.repo.createRecord',
+            headers: {
+                'Content-Type': 'application/json; charset=UTF-8',
+                'Authorization': 'Bearer ' + client._session.accessJwt,
+            },
+            fetch: true,
+            data: JSON.stringify({
+                repo: client._session.did,
+                collection: 'app.bsky.feed.post',
+                record: postData,
+            }),
+        };
 
         if (confirm("ブックマーク「"+JSON.stringify(postData.text)+"」をBlueskyに投稿します")) {
             return new Promise((resolve, reject) => {
-                GM_xmlhttpRequest({
-                    method: "POST",
-                    url: BLUESKY_PDS_URL + '/xrpc/com.atproto.repo.createRecord',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Authorization': 'Bearer ' + client._session.accessJwt,
-                    },
-                    fetch: true,
-                    data: JSON.stringify({
-                        repo: client._session.did,
-                        collection: 'app.bsky.feed.post',
-                        record: postData,
-                    }),
-                    onload: ({response}) => {
-                        if (response.OK) {
-                            resolve(response);
-                        }
-                        else {
-                            window.alert('Blueskyへの投稿に失敗しました');
-                            reject(response);
-                        }
-                    },
-                    withCredentials: true,
-                    responseType: 'json',
-                });
+                GM_xmlhttpRequest(jsonText);
             });
         }
         return Promise.reject();
@@ -119,88 +100,11 @@ async function postBluesky(bookmarkNode) {
 }
 
 /**
- * BlueskyのPDSにBLOBデータを投稿する
- * @param postBlobData BLOBデータ
- * @return 実行結果
- */
-async function postBlobData(postBlobData) {
-    try {
-        let client = new BlueskyConnect(BLUESKY_HANDLE, BLUESKY_APP_PASS);
-        await client.verify_session()
-            .then((session) => {
-            if (session.error)
-            {
-                throw new Error(session.message);
-            }
-            GM_setValue('bsky_session', session);
-        });
-
-        return new Promise((resolve, reject) => {
-            GM_xmlhttpRequest({
-                method: "POST",
-                url: BLUESKY_PDS_URL + '/xrpc/com.atproto.repo.uploadBlob',
-                headers: {
-                    'Content-Type': postBlobData.type,
-                    'Authorization': 'Bearer ' + client._session.accessJwt,
-                },
-                fetch: true,
-                data: postBlobData,
-                onload: ({response}) => {
-                    if (response.OK) {
-                        resolve(response);
-                    }
-                    else {
-                        console.log('BLOBアップロードに失敗しました');
-                        reject(response);
-                    }
-                },
-                withCredentials: true,
-                responseType: 'json',
-            });
-        });
-    }
-    catch (e) {
-        console.log(e);
-    }
-}
-
-/**
- * ブックマーク投稿用データを作成して返す
- * @param bookmarkNode ブックマークアイテムのノード
- * @return ブックマーク投稿用データ
- */
-async function createBookmarkPostData(bookmarkNode) {
-    const postComment = getBaseComment(bookmarkNode);
-    const now = (new Date()).toISOString();
-    const linkUrl = getBookmarkUrl(bookmarkNode);
-    const linkText = getBookmarkTitle(bookmarkNode);
-    const linkDescription = getBookmarkDescription(bookmarkNode);
-    const imageData = await getBookmarkImage(bookmarkNode);
-
-    // 画像をPDSに投稿
-    const blobData = await postBlobData(imageData).blob;
-
-    return {
-        'text': postComment,
-        'createdAt': now,
-        'embed': {
-            '$type': 'app.bsky.embed.external',
-            'external': {
-                'uri': linkUrl,
-                'title': linkText,
-                'description': linkDescription,
-                'thumb': blobData,
-            },
-        },
-    };
-}
-
-/**
- * テキスト投稿用データを作成して返す
+ * Blueskyの投稿用データを作成して返す
  * @param bookmarkNode ブックマークアイテムのノード
  * @return 投稿用データ
  */
-function createTextPostData(bookmarkNode) {
+function createPostData(bookmarkNode) {
     const baseComment = getBaseComment(bookmarkNode);
     const linkText = getBookmarkTitle(bookmarkNode);
     const linkUrl = getBookmarkUrl(bookmarkNode);
@@ -291,124 +195,8 @@ function getBookmarkTitle(bookmarkNode) {
 }
 
 /**
- * ブックマークの説明文を返す
- * @param bookmarkNode ブックマークアイテムのノード
- * @return ブックマークの説明文
- */
-function getBookmarkDescription(bookmarkNode) {
-    return getNodeText(bookmarkNode, "p.centerarticle-entry-summary");
-}
-
-/**
- * ブックマークイメージを返す
- * @param bookmarkNode ブックマークアイテムのノード
- * @return ブックマークイメージ
- */
-async function getBookmarkImage(bookmarkNode) {
-    // OG:imageから取る場合は@connectで*を指定すること
-    //const bookmarkUrl = getBookmarkUrl(bookmarkNode);
-    //const imageUrl = getOgImage(bookmarkUrl);
-
-    // 通常はサムネ画像を取得
-    let imageNode = getNodeImage(bookmarkNode, "a.centerarticle-entry-image img");
-    if (!imageNode) {
-        // サムネ画像が取得できない場合はfavicon画像を取得
-        imageNode = getNodeImage(bookmarkNode, "img.centerarticle-entry-favicon");
-    }
-
-    const imageUrl = getAttrText(imageNode, "src");
-    const fileName = getFileName(imageUrl);
-    const fileType = getFileType(fileName);
-    const imageBlob = await getImageBlob(imageUrl);
-
-    return imageBlob;
-}
-
-/**
- * 画像BLOBデータを返す
- * @param imageUrl 画像URL
- * @return 画像BLOBデータ
- */
-async function getImageBlob(imageUrl) {
-    return new Promise((resolve, reject) => {
-        GM_xmlhttpRequest({
-            method: "GET",
-            url: imageUrl,
-            onload: ({response}) => {
-                const arrayBuffer = response;
-                resolve(arrayBuffer);
-            },
-            withCredentials: true,
-            responseType: 'blob',
-        });
-    });
-}
-
-/**
- * OGデータから画像URLを返す
- * @param url OGデータを取得するURL
- * @return 画像URL
- */
-function getOgImage(url) {
-    const ogData = fetch(url).then(res => res.text()).then(text => {
-        const el = new DOMParser().parseFromString(text, "text/html");
-        const headEls = (el.head.children);
-        Array.from(headEls).map(v => {
-            const prop = v.getAttribute('property');
-            if (!prop) {
-                return
-            };
-        });
-    });
-    return (ogData ? "" : ogData.image) ?? "";
-}
-
-/**
- * ファイル名を返す
- * @param url 対象URL
- * @return ファイル名
- */
-function getFileName(url) {
-    return url.substring(url.lastIndexOf("/"));
-}
-
-/**
- * ファイル形式を返す
- * @param fileName ファイル名
- * @return ファイル形式
- */
-function getFileType(fileName) {
-    const fileExtension = fileName.substring(fileName.indexOf(".")).toLowerCase();
-    switch (fileExtension) {
-        case "jpg":
-        case "jpeg":
-            return "image/jpeg";
-        case "png":
-            return "image/png";
-        case "gif":
-            return "image/gif";
-        default:
-            return "";
-    }
-}
-
-/**
- * タグを返す
- * @param baseNode ノード
- * @param selectors タグの取得ルール
- * @return タグの値
- */
-function getNodeImage(baseNode, selectors) {
-    const node = baseNode.querySelector(selectors);
-    if (node) {
-        return node;
-    }
-    return null;
-}
-
-/**
  * タグの値を返す
- * @param baseNode ノード
+ * @param bookmarkNode ブックマークアイテムのノード
  * @param selectors タグの取得ルール
  * @return タグの値
  */
@@ -422,7 +210,7 @@ function getNodeText(baseNode, selectors) {
 
 /**
  * 属性の値を返す
- * @param baseNode ノード
+ * @param bookmarkNode ブックマークアイテムのノード
  * @param attrName 属性名
  * @return 属性の値
  */
